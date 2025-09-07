25246be6578d107bb3ad92c72ebd49d72bb17854
brandtbuchwald-rgb <brandt.buchwald@gmail.com>
2025-09-07 09:31:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("Gear Optimizer loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  const statTotals = {
+    Attack: 0,
+    "Attack Speed": 0,
+    "Crit Chance": 0,
+    Evasion: 0,
+    "Critical Damage": 0,
+    Defence: 0,
+    HP: 0,
+  };
+
+  const statElements = {};
+  Object.keys(statTotals).forEach(stat => {
+    statElements[stat] = document.getElementById(stat.replace(" ", "") + "-total");
+  });
+
+  function updateTotals() {
+    // reset totals
+    for (let stat in statTotals) {
+      statTotals[stat] = 0;
+    }
+
+    // loop through all selects
+    document.querySelectorAll(".gear-slot select").forEach(select => {
+      const value = select.value;
+      if (value !== "-- Select Stat --") {
+        const [stat, num] = value.split(" +");
+        if (statTotals[stat] !== undefined) {
+          statTotals[stat] += parseInt(num);
+        }
+      }
+    });
+
+    // update UI
+    for (let stat in statTotals) {
+      statElements[stat].textContent = statTotals[stat] + "%";
+    }
+  }
+
+  // attach event listener
+  document.querySelectorAll(".gear-slot select").forEach(select => {
+    select.addEventListener("change", updateTotals);
+  });
+});
